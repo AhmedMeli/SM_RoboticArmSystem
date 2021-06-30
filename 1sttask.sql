b1b890c179aa3067ee55ffed1d8383389d201d13
AhmedMeli <67350229+AhmedMeli@users.noreply.github.com>
2021-07-01 01:28:07 +0300

    README and the DB

---
+++ 1sttask.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2021 at 12:27 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `1sttask`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `motors`
+--
+
+CREATE TABLE `motors` (
+  `RobotID` int(10) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `motor1` int(10) NOT NULL,
+  `motor2` int(10) NOT NULL,
+  `motor3` int(10) NOT NULL,
+  `motor4` int(10) NOT NULL,
+  `motor5` int(10) NOT NULL,
+  `motor6` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `motors`
+--
+
+INSERT INTO `motors` (`RobotID`, `status`, `motor1`, `motor2`, `motor3`, `motor4`, `motor5`, `motor6`) VALUES
+(1, 1, 180, 0, 180, 0, 0, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `motors`
+--
+ALTER TABLE `motors`
+  ADD PRIMARY KEY (`RobotID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SM_RoboticArmSystem
+
+This project is for SmartMethods done by Ahmed Melibari
  
